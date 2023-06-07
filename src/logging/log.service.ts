715d5ca83cbdc4f64125bd2c6b8cef01a4df2766
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2023-06-07 11:03:56 +0200

    console.log removed

--- src/logging/log.service.ts
+++ src/logging/log.service.ts
@@ -1,6 +1,6 @@
 import { ConsoleLogger, Injectable, Scope } from '@nestjs/common'
 import { LogInterface, OptionalLogParameters, Severity } from './log.interface'
-import { white } from 'ansi-colors'
+import chalk from 'chalk'
 import { HttpLogSenderService } from './http-log-sender.service'
 
 @Injectable({
@@ -10,9 +10,7 @@ export class LogService implements LogInterface {
   private _context = 'unknown'
   private _nestLogger = new ConsoleLogger()
 
-  constructor(private readonly _httpLogger: HttpLogSenderService) {
-    console.log(`LogService constructor`)
-  }
+  constructor(private readonly _httpLogger: HttpLogSenderService) {}
 
   set context(value: string) {
     this._context = value
@@ -24,10 +22,13 @@ export class LogService implements LogInterface {
     logService.context = context
   }
 
-  format(message: string, params?: OptionalLogParameters) {
+  formatForConsole(message: string, params?: OptionalLogParameters) {
     const uuidPart = params?.uuid ? `(${params?.uuid})` : ''
-    const metaPart = params?.meta ? white('metadata: ' + JSON.stringify(params?.meta)) : ''
-    const allDefined = [uuidPart, message, metaPart].filter(s => s !== undefined)
+    const metaPart = params?.meta ? chalk.yellow('metadata: ' + JSON.stringify(params?.meta)) : ''
+    let _message = message
+    if (params?.emphasizeInConsole === 'color') _message = chalk.white(message)
+    if (params?.emphasizeInConsole === 'startup') _message = `=-= ${message} =-=`
+    const allDefined = [uuidPart, _message, metaPart].filter(s => s !== undefined)
     return allDefined.join(' ')
   }
 
@@ -46,32 +47,35 @@ export class LogService implements LogInterface {
   }
 
   info(message: string, params?: OptionalLogParameters) {
-    this._nestLogger.log(this.format(message, params), params?.context ?? this._context)
+    this._nestLogger.log(this.formatForConsole(message, params), params?.context ?? this._context)
     if (!params?.consoleOnly) this.logToHttp(Severity.info, message, params)
   }
 
   error(message: string, params?: OptionalLogParameters) {
-    this._nestLogger.error(this.format(message, params), params?.context ?? this._context)
+    this._nestLogger.error(this.formatForConsole(message, params), params?.context ?? this._context)
     if (!params?.consoleOnly) this.logToHttp(Severity.error, message, params)
   }
 
   urgent(message: string, params?: OptionalLogParameters) {
-    this._nestLogger.error(this.format(message, params), params?.context ?? this._context)
+    this._nestLogger.error(this.formatForConsole(message, params), params?.context ?? this._context)
     if (!params?.consoleOnly) this.logToHttp(Severity.urgent, message, params)
   }
 
   warn(message: string, params?: OptionalLogParameters) {
-    this._nestLogger.warn(this.format(message, params), params?.context ?? this._context)
+    this._nestLogger.warn(this.formatForConsole(message, params), params?.context ?? this._context)
     if (!params?.consoleOnly) this.logToHttp(Severity.warn, message, params)
   }
 
   debug(message: string, params?: OptionalLogParameters) {
-    this._nestLogger.debug(this.format(message, params), params?.context ?? this._context)
+    this._nestLogger.debug(this.formatForConsole(message, params), params?.context ?? this._context)
     if (!params?.consoleOnly) this.logToHttp(Severity.debug, message, params)
   }
 
   verbose(message: string, params?: OptionalLogParameters) {
-    this._nestLogger.verbose(this.format(message, params), params?.context ?? this._context)
+    this._nestLogger.verbose(
+      this.formatForConsole(message, params),
+      params?.context ?? this._context,
+    )
     if (!params?.consoleOnly) this.logToHttp(Severity.verbose, message, params)
   }
 }

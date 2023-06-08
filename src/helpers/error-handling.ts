d928ac8c629f18472adaeaba16fb6c78202bdd6d
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2023-06-08 10:13:28 +0200

    Improvement in error handling

--- src/helpers/error-handling.ts
+++ src/helpers/error-handling.ts
@@ -1,29 +1,35 @@
-export function ensureError(value: unknown): Error {
-  if (value instanceof Error) return value
+export function ensureError<E = Error>(value: unknown): E {
+  if (value instanceof Error) return value as E
 
   let stringified = '[Unable to stringify the thrown value]'
   try {
     stringified = JSON.stringify(value)
   } catch {}
 
-  return new Error(stringified)
+  return new Error(stringified) as E
 }
 
-export async function to<T>(
+export type FailedTuple<E = Error> = [E, undefined]
+export type SuccessTuple<T> = [null, T]
+export type FailedOrSuccessTuple<T, E = Error> = FailedTuple<E> | SuccessTuple<T>
+export type handleErrorFn<T, E = Error> = (error: E) => FailedOrSuccessTuple<T, E>
+const processCatch = <T, E = Error>(e: unknown, handleError?: handleErrorFn<T, E>) => {
+  if (handleError) return handleError(ensureError(e))
+  return [ensureError(e), undefined] as FailedOrSuccessTuple<T, E>
+}
+
+export async function to<T, E = Error>(
   promise: Promise<T>,
-  handleError?: (error: Error) => [null | Error, T | undefined],
-): Promise<[null | Error, T | undefined]> {
+  handleError?: handleErrorFn<T, E>,
+): Promise<FailedOrSuccessTuple<T, E>> {
   return promise
-    .then((res: T) => [null, res] as [null, T])
-    .catch((e: unknown) => {
-      if (handleError) return handleError(ensureError(e))
-      return [ensureError(e), undefined]
-    })
+    .then((result: T) => [null, result] as SuccessTuple<T>)
+    .catch((e: unknown) => processCatch<T, E>(e, handleError))
 }
 
 export function toSync<T>(
   canThrow: () => T,
-  handleError?: (error: Error) => [null | Error, T | undefined],
+  handleError?: handleErrorFn<T>,
 ): [null | Error, T | undefined] {
   try {
     return [null, canThrow()]
